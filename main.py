21190ff75955087b2c07a0852b1629d452e82150
Mirko Dugajlic <mirkodugi888@gmail.com>
2024-05-20 14:09:11 +0200

    Implemented frequencies and probability functions

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ extension) of your favorite song and read the contents of the file into a
 byte array.
 • Change bytes to bits to obtain a representation of the wav file as a string
 of bits.
-• Split the obtained sequence of bits into blocks of 8, and find the frequecy
+• Split the obtained sequence of bits into blocks of 8, and find the frequency
 of every possible block, and use the obtained frequencies to estimate the
 probability of all possible blocks of 8 bits.
 • Use the estimated probability distribution to obtain a Huffman code for
@@ -27,15 +27,33 @@ def bytes_to_bits_binary(byte_data):
     bits_data = bin(int.from_bytes(byte_data, byteorder='big'))[2:]
     return bits_data
 
+
 def split_to_blocks(bits_data):
     chunks, chunk_size = len(bits_data), 8
     num_blocks = chunks // chunk_size
-    blocks_array = [''] * (num_blocks+1)
+    blocks_array = [''] * (num_blocks + 1)
     j = 0
     for i in range(0, chunks, chunk_size):
-        blocks_array[j] = bits_data[i:i+chunk_size]
+        blocks_array[j] = bits_data[i:i + chunk_size]
         j = j + 1
     return blocks_array
+
+
+def extract_frequencies(blocks_array):
+    frequencies = [] * len(blocks_array)
+    for i, block in enumerate(blocks_array):
+        # print(f"Block {i}: {block}")
+        no_of_ones = block.count('1')
+        no_of_zeros = block.count('0')
+        frequencies.append((no_of_ones, no_of_zeros))
+    return frequencies
+
+
+def probability(frequencies_tuples):
+    probabilities = [] * len(frequencies_tuples)
+    for freq in frequencies_tuples:
+        probabilities.append((freq[0] / 8, freq[1] / 8))
+    return probabilities
 
 
 if __name__ == '__main__':
@@ -44,7 +62,10 @@ if __name__ == '__main__':
 
     bits_array = bytes_to_bits_binary(wav_byte_array)
     bit_blocks = split_to_blocks(bits_array)
-
+    frequencies = extract_frequencies(bit_blocks)  # This variable is an array of tuples for some i-th element of
+    # bit_blocks array there is corresponding tuple of (number of ones, number of zeros) in frequencies
+    probabilities = probability(frequencies)  # Same as for frequencies but this gives probabilities ->
+    # i.e. number of  ones / 8
     print("1")
 
 # Write to file if needed but file is too long
